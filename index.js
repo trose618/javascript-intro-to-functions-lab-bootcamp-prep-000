07fd3dcfa0193cbd3794926e81fb4815ca2564de
Terrance Rose Jr <trose618@gmail.com>
2018-09-25 10:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  if(string==="I love you, Grandma."){
+    return "I love you, too."
+  }
   if(string.toLowerCase()===string){
     return "I can't hear you!"
+  }else if (string.toUpperCase()===string){
+    return "YES INDEED!"
   }
 }
